7cd16fe145ab10b8dc5cfdb4074774b411fde9dd
Darin Hajou <DarinHajou@gmail.com>
2025-09-04 16:38:55 +0200

    refactor(notifications): delete deprecated createNotification handler

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -2,24 +2,6 @@ import { Request, Response } from "express";
 import * as notificationService from '../services/notification.service';
 import logger from "../config/loggingConfig";
 
-export const createNotification = async (req: Request, res: Response) => {
-  const authUser = req.currentUser;
-
-  // Check if authUser is defined
-  if (!authUser) {
-    logger.warn('No authenticated user found when trying to create notification.');
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  const { reason } = req.body;
-  try {
-    const notification = await notificationService.addNotification(authUser?.pi_uid, reason);
-    return res.status(200).json({ message: "Notification created successfully", notification });
-  } catch (error) {
-    logger.error('Failed to create notification', error);
-    return res.status(500).json({ message: 'An error occurred while creating notification; please try again later' });
-  }
-};
-
 export const getNotifications = async (req: Request, res: Response) => {
   const authUser = req.currentUser;
 
